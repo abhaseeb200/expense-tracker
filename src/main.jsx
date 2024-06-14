0f3d9dff5197ea1c4f27b8558e377b0fb0cb056b
Abdul Haseeb <haseebabdul747@gmail.com>
2024-06-14 18:07:11 +0500

    integrate the redux

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,15 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { Provider } from "react-redux";
+import { PersistGate } from "redux-persist/integration/react";
 import App from "./App.jsx";
+import { persistor, store } from "./config/store.js";
 import "./index.css";
 
-ReactDOM.createRoot(document.getElementById("root")).render(<App />);
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <Provider store={store}>
+    <PersistGate loading={null} persistor={persistor}>
+      <App />
+    </PersistGate>
+  </Provider>
+);
