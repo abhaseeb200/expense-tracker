900548d3dea9b5b9c9a4ee3e9128e49d0075b471
Abdul Haseeb <haseebabdul747@gmail.com>
2024-06-17 01:48:59 +0500

    create a reducer action file for budget

---
+++ src/feature/budget/budgetSlice.js
@@ -0,0 +1,54 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+export const budgetSlice = createSlice({
+  name: "budget",
+  initialState: { budgetData: [] },
+  reducers: {
+    getBudget: (state, action) => {
+      return {
+        ...state,
+        budgetData: [...action.payload],
+      };
+    },
+    addBudget: (state, action) => {
+      return {
+        ...state,
+        budgetData: [...state.budgetData, action.payload],
+      };
+    },
+    editBudget: (state, action) => {
+      let currentIndex = state.budgetData.findIndex(
+        (item) => item.docID == action.payload.docID
+      );
+
+      if (currentIndex !== -1) {
+        state.budgetData[currentIndex] = action.payload;
+      }
+      return state;
+    },
+    deleteBudget: (state, action) => {
+      return {
+        ...state,
+        budgetData: state.budgetData.filter(
+          (item) => item.docID != action.payload
+        ),
+      };
+    },
+    clearBudget: (state) => {
+      return {
+        ...state,
+        budgetData: [],
+      };
+    },
+  },
+});
+
+export const {
+  getBudget,
+  addBudget,
+  editBudget,
+  deleteBudget,
+  clearBudget,
+} = budgetSlice.actions;
+
+export default budgetSlice.reducer;
