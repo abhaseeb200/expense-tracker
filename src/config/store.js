8b2637603a1903bfe21ae8611aa125fd6e39631d
Abdul Haseeb <haseebabdul747@gmail.com>
2024-06-17 01:44:47 +0500

    modified store

--- src/config/store.js
+++ src/config/store.js
@@ -2,6 +2,9 @@ import { combineReducers, createStore } from "@reduxjs/toolkit";
 import { persistStore, persistReducer } from "redux-persist";
 import storage from "redux-persist/lib/storage";
 import userSlice from "../feature/auth/userSlice";
+import transactionSlice from "../feature/transaction/transactionSlice";
+import categorySlice from "../feature/category/categorySlice";
+import budgetSlice from "../feature/budget/budgetSlice";
 
 const persistConfig = {
   key: "root",
@@ -10,6 +13,9 @@ const persistConfig = {
 
 const reducers = combineReducers({
   auth: userSlice,
+  transaction: transactionSlice,
+  category: categorySlice,
+  budget: budgetSlice,
 });
 
 const persistedReducer = persistReducer(persistConfig, reducers);
