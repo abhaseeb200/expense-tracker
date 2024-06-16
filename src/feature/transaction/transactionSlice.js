7daa62344ebee7fa12d4d43f37e12766d2f736bd
Abdul Haseeb <haseebabdul747@gmail.com>
2024-06-17 01:49:24 +0500

    create a reducer action file for transaction

---
+++ src/feature/transaction/transactionSlice.js
@@ -0,0 +1,54 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+export const transactionSlice = createSlice({
+  name: "transaction",
+  initialState: { transactionData: [] },
+  reducers: {
+    getTransaction: (state, action) => {
+      return {
+        ...state,
+        transactionData: [...action.payload],
+      };
+    },
+    addTransaction: (state, action) => {
+      return {
+        ...state,
+        transactionData: [...state.transactionData, action.payload],
+      };
+    },
+    editTransaction: (state, action) => {
+      let currentIndex = state.transactionData.findIndex(
+        (item) => item.docID == action.payload.docID
+      );
+
+      if (currentIndex !== -1) {
+        state.transactionData[currentIndex] = action.payload;
+      }
+      return state;
+    },
+    deleteTransaction: (state, action) => {
+      return {
+        ...state,
+        transactionData: state.transactionData.filter(
+          (item) => item.docID != action.payload
+        ),
+      };
+    },
+    clearTransactions: (state) => {
+      return {
+        ...state,
+        transactionData: [],
+      };
+    },
+  },
+});
+
+export const {
+  getTransaction,
+  addTransaction,
+  editTransaction,
+  deleteTransaction,
+  clearTransactions,
+} = transactionSlice.actions;
+
+export default transactionSlice.reducer;
