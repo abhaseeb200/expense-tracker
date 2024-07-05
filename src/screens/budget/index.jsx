9e5491fd001875c4fab856c4ecde8f97a990bf72
Abdul Haseeb <haseebabdul747@gmail.com>
2024-07-05 21:07:09 +0500

    remove the useless code

--- src/screens/budget/index.jsx
+++ src/screens/budget/index.jsx
@@ -1,83 +1,30 @@
 import { useEffect, useState } from "react";
-import { useDispatch, useSelector } from "react-redux";
-import { useOutletContext } from "react-router-dom";
-import { toast } from "react-toastify";
+import { useSelector } from "react-redux";
 import {
   Button,
   Card,
   CardBody,
-  CardText,
   CardTitle,
-  Container,
-  Label,
   Modal,
   ModalBody,
   ModalFooter,
   ModalHeader,
   Spinner,
 } from "reactstrap";
-import { CustomInput, Input } from "../../components/input";
+import { Input } from "../../components/input";
 import Table from "../../components/table";
-// import {
-//   deleteBudget as firebaseDeleteBudget,
-//   getBudget as firebaseGetBudget,
-//   setBudget,
-//   updateBudget as firebaseUpdateBudget,
-// } from "../../config/service/firebase/budget";
-// import {
-//   addBudget,
-//   deleteBudget,
-//   editBudget,
-//   getBudget,
-// } from "../../feature/budget/budgetSlice";
-import useBudget from "../../hooks/useBudget";
-import BudgetColumns from "../../config/constant/budgetColumns";
 import Search from "../../components/Search";
-
-const inputs = [
-  {
-    id: 1,
-    name: "name",
-    type: "text",
-    placeholder: "Bills.",
-    label: "name",
-  },
-  {
-    id: 2,
-    name: "date",
-    type: "date",
-    label: "date",
-    max: new Date().toISOString().split("T")[0],
-  },
-  {
-    id: 3,
-    name: "amount",
-    type: "number",
-    placeholder: "200...",
-    label: "amount",
-    required: true,
-    min: 1,
-    pattern: "/^[1-9]d*$/",
-    errorMessage: "Amount must be greater than 01",
-  },
-];
+import useBudget from "../../hooks/useBudget";
+import budgetInputs from "../../config/constant/budgetInputs";
+import budgetColumns from "../../config/constant/budgetColumns";
 
 const Budget = () => {
-  // const [loader, setLoader] = useState(false);
-  const [tableLoader, setTableLoader] = useState(false);
   const [currentDocID, setCurrentDocID] = useState("");
-  const [saveLoader, setSaveLoader] = useState(false);
-  const [deleteLoader, setDeleteLoader] = useState(false);
   const [isUpdate, setIsUpdate] = useState(false);
   const [isOpenModal, setIsOpenModal] = useState(false);
   const [sortConfig, setSortConfig] = useState({ key: "", direction: "" });
   const [backUp, setBackUp] = useState([]);
-
-  const [values, setValues] = useState({
-    name: "",
-    date: "",
-    amount: "",
-  });
+  const [values, setValues] = useState({});
   const [errors, setErrors] = useState({});
 
   const {
@@ -89,268 +36,8 @@ const Budget = () => {
     loading,
   } = useBudget();
 
-  const [name, setName] = useState({
-    value: "",
-    isError: false,
-    messageError: "",
-  });
-
-  const [date, setDate] = useState({
-    value: "",
-    isError: false,
-    messageError: "",
-  });
-
-  const [amount, setAmount] = useState({
-    value: "",
-    isError: false,
-    messageError: "",
-  });
-
-  const dispatch = useDispatch();
   const { budgetData } = useSelector((state) => state?.budget);
-  const { userData } = useSelector((state) => state.auth);
-
-  console.log({ budgetData });
-
-  const [currentUserID] = useOutletContext();
-
-  const nameHandler = (e) => {
-    let expVal = e.target.value.trim().toLowerCase();
-    if (expVal === "") {
-      setName({
-        value: e.target.value,
-        isError: true,
-        messageError: "Please provide name",
-      });
-    } else {
-      setName({
-        value: e.target.value,
-        isError: false,
-        messageError: "",
-      });
-    }
-  };
-
-  const dateHandler = (e) => {
-    if (e.target.value === "") {
-      setDate({
-        value: e.target.value,
-        isError: true,
-        messageError: "Please select date",
-      });
-    } else {
-      setDate({
-        value: e.target.value,
-        isError: false,
-        messageError: "",
-      });
-    }
-  };
-
-  const amountHandler = (e) => {
-    if (e.target.value === "") {
-      setAmount({
-        value: e.target.value,
-        isError: true,
-        messageError: "Please provide amount",
-      });
-    } else if (e.target.value <= 0) {
-      setAmount({
-        value: e.target.value,
-        isError: true,
-        messageError: "Amount must be greater than 0",
-      });
-    } else {
-      setAmount({
-        value: e.target.value,
-        isError: false,
-        messageError: "",
-      });
-    }
-  };
-
-  const addBudgetHandler = () => {
-    if (name.value === "") {
-      setName({
-        value: name.value,
-        isError: true,
-        messageError: "Please provide name",
-      });
-    }
-    if (date.value === "") {
-      setDate({
-        value: date.value,
-        isError: true,
-        messageError: "Please select date",
-      });
-    }
-    if (amount.value === "") {
-      setAmount({
-        value: amount.value,
-        isError: true,
-        messageError: "Please provide amount",
-      });
-    }
-
-    if (name.value === "" || date.value === "" || amount.value === "") {
-      return;
-    }
-
-    //check validation
-    if (!name.isError && !date.isError && !amount.isError) {
-      setLoader(true);
-      setBudget(name.value, date.value, amount.value, currentUserID)
-        .then((res) => {
-          setLoader(false);
-          let data = {
-            docID: res.id,
-            docData: {
-              name: name.value,
-              date: date.value,
-              amount: amount.value,
-            },
-          };
-          dispatch(addBudget(data));
-          toast.success("Budget add successfully!", {
-            autoClose: 1500,
-          });
-        })
-        .catch((err) => {
-          setLoader(false);
-          toast.errors(err?.message, {
-            autoClose: 1500,
-          });
-        });
-    }
-  };
-
-  const getBudgetHandler = () => {
-    setTableLoader(true);
-    firebaseGetBudget(currentUserID)
-      .then((res) => {
-        let tempBudgetData = [];
-        res.forEach((element) => {
-          tempBudgetData.push({
-            docID: element.id,
-            docData: element.data(),
-          });
-        });
-        setTableLoader(false);
-        dispatch(getBudget(tempBudgetData));
-      })
-      .catch((err) => {
-        setTableLoader(false);
-        toast.errors(err?.message, {
-          autoClose: 1500,
-        });
-      });
-  };
-
-  const deleteHandler = async (item) => {
-    console.log(item);
-    setDeleteLoader(true);
-    setCurrentDocID(item?.docID);
-    try {
-      await firebaseDeleteBudget(item?.docID);
-      dispatch(deleteBudget(item?.docID));
-      setDeleteLoader(false);
-      restAllFields();
-      toast.success("Delete budget successfully!", {
-        autoClose: 1500,
-      });
-    } catch (errors) {
-      setDeleteLoader(false);
-      toast.errors(errors.message, {
-        autoClose: 1500,
-      });
-    }
-  };
-
-  const editHandler = (item) => {
-    setName({
-      value: item.docData.name,
-      isError: false,
-      messageError: "",
-    });
-    setDate({
-      value: item.docData.date,
-      isError: false,
-      messageError: "",
-    });
-    setAmount({
-      value: item.docData.amount,
-      isError: false,
-      messageError: "",
-    });
-    setCurrentDocID(item.docID);
-    setIsUpdate(true);
-  };
-
-  const saveHandler = async () => {
-    console.log(currentDocID);
-    if (name.value === "" || amount.value === "" || date.value === "") {
-      return;
-    }
-
-    //CHECK VALIDATION
-    if (!name.isError && !amount.isError && !date.isError) {
-      setSaveLoader(true);
-      try {
-        await firebaseUpdateBudget(
-          name.value,
-          amount.value,
-          date.value,
-          currentDocID
-        );
-        let data = {
-          docID: currentDocID,
-          docData: { name: name.value, amount: amount.value, date: date.value },
-        };
-        dispatch(editBudget(data));
-        toast.success("Update Budget successfully!", {
-          autoClose: 1500,
-        });
-        setIsUpdate(false);
-        setSaveLoader(false);
-        restAllFields();
-      } catch (errors) {
-        toast.errors(errors?.message, {
-          autoClose: 1500,
-        });
-      }
-    }
-  };
-
-  const cancelHandler = () => {
-    restAllFields();
-    setIsUpdate(false);
-  };
-
-  const restAllFields = () => {
-    setName({
-      value: "",
-      isError: false,
-      messageError: "",
-    });
-    setDate({
-      value: "",
-      isError: false,
-      messageError: "",
-    });
-    setAmount({
-      value: "",
-      isError: false,
-      messageError: "",
-    });
-  };
-
-  const todayDateAttributeHandler = () => {
-    let d = new Date();
-    return (
-      d.getFullYear() + "-" + parseInt(d.getMonth() + 1) + "-" + d.getDate()
-    );
-  };
+  const { userData } = useSelector((state) => state?.auth);
 
   const handleDelete = async (data) => {
     setCurrentDocID(data?.docID);
@@ -403,8 +90,6 @@ const Budget = () => {
   const handleSubmit = async (e) => {
     e.preventDefault();
 
-    // return console.log(e.target.reset);
-
     const data = {};
     let error = {};
     let formData = new FormData(e.target);
@@ -420,7 +105,6 @@ const Budget = () => {
 
     //SUBMIT THE FORM BY USING 'DATA'
     if (!Object.values(error).includes(true)) {
-      //REQUIRED BODY KEYS
       let body = {
         userId: userData?.userId,
         timeStamp: Date.now(),
@@ -473,7 +157,7 @@ const Budget = () => {
     if (!budgetData?.length) {
       useGetBudget();
     }
-  }, [currentUserID]);
+  }, []);
 
   return (
     <>
@@ -494,7 +178,7 @@ const Budget = () => {
             onUpdate={handleUpdate}
             onSort={handleOnSort}
             sortConfig={sortConfig}
-            columns={BudgetColumns}
+            columns={budgetColumns}
             rows={backUp}
             loading={initLoading}
             iconLoading={loading}
@@ -512,7 +196,7 @@ const Budget = () => {
         <ModalHeader>{isUpdate ? "Update Budget" : "Add Budget"}</ModalHeader>
         <form onSubmit={handleSubmit} className="d-flex flex-column">
           <ModalBody className="gap-3 d-flex flex-column">
-            {inputs?.map((input) => {
+            {budgetInputs?.map((input) => {
               return (
                 <Input
                   key={input?.id}
