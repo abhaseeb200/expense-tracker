b5f8e09e52502a1410de7acc071472885e5cae8a
Abdul Haseeb <haseebabdul747@gmail.com>
2024-07-21 21:00:01 +0500

    modified dashboard screen

--- src/components/icons/index.jsx
+++ src/components/icons/index.jsx
@@ -58,3 +58,99 @@ export const ChevronIcon = (props) => {
     </span>
   );
 };
+
+export const DollarIcon = (props) => {
+  const { className, ...otherProps } = props;
+
+  return (
+    <div className={className}>
+      <svg
+        {...otherProps}
+        xmlns="http://www.w3.org/2000/svg"
+        viewBox="0 0 24 24"
+      >
+        <path d="M15.999 8.5h2c0-2.837-2.755-4.131-5-4.429V2h-2v2.071c-2.245.298-5 1.592-5 4.429 0 2.706 2.666 4.113 5 4.43v4.97c-1.448-.251-3-1.024-3-2.4h-2c0 2.589 2.425 4.119 5 4.436V22h2v-2.07c2.245-.298 5-1.593 5-4.43s-2.755-4.131-5-4.429V6.1c1.33.239 3 .941 3 2.4zm-8 0c0-1.459 1.67-2.161 3-2.4v4.799c-1.371-.253-3-1.002-3-2.399zm8 7c0 1.459-1.67 2.161-3 2.4v-4.8c1.33.239 3 .941 3 2.4z"></path>
+      </svg>
+    </div>
+  );
+};
+
+export const TransferIcon = (props) => {
+  const { className, ...otherProps } = props;
+
+  return (
+    <div className={className}>
+      <svg
+        {...otherProps}
+        xmlns="http://www.w3.org/2000/svg"
+        viewBox="0 0 24 24"
+      >
+        <path d="M19.924 10.383a1 1 0 0 0-.217-1.09l-5-5-1.414 1.414L16.586 9H4v2h15a1 1 0 0 0 .924-.617zM4.076 13.617a1 1 0 0 0 .217 1.09l5 5 1.414-1.414L7.414 15H20v-2H5a.999.999 0 0 0-.924.617z"></path>
+      </svg>
+    </div>
+  );
+};
+
+export const SaveMoneyIcon = (props) => {
+  const { className, ...otherProps } = props;
+
+  return (
+    <div className={className}>
+      <svg
+        {...otherProps}
+        xmlns="http://www.w3.org/2000/svg"
+        viewBox="0 0 24 24"
+      >
+        <path d="M12 22c3.976 0 8-1.374 8-4V6c0-2.626-4.024-4-8-4S4 3.374 4 6v12c0 2.626 4.024 4 8 4zm0-2c-3.722 0-6-1.295-6-2v-1.268C7.541 17.57 9.777 18 12 18s4.459-.43 6-1.268V18c0 .705-2.278 2-6 2zm0-16c3.722 0 6 1.295 6 2s-2.278 2-6 2-6-1.295-6-2 2.278-2 6-2zM6 8.732C7.541 9.57 9.777 10 12 10s4.459-.43 6-1.268V10c0 .705-2.278 2-6 2s-6-1.295-6-2V8.732zm0 4C7.541 13.57 9.777 14 12 14s4.459-.43 6-1.268V14c0 .705-2.278 2-6 2s-6-1.295-6-2v-1.268z"></path>
+      </svg>
+    </div>
+  );
+};
+
+export const WalletIcon = (props) => {
+  const { className, ...otherProps } = props;
+
+  return (
+    <div className={className}>
+      <svg
+        {...otherProps}
+        xmlns="http://www.w3.org/2000/svg"
+        viewBox="0 0 24 24"
+      >
+        <path d="M20 3H5C3.346 3 2 4.346 2 6v12c0 1.654 1.346 3 3 3h15c1.103 0 2-.897 2-2V5c0-1.103-.897-2-2-2zM5 19c-.552 0-1-.449-1-1V6c0-.551.448-1 1-1h15v3h-6c-1.103 0-2 .897-2 2v4c0 1.103.897 2 2 2h6.001v3H5zm15-9v4h-6v-4h6z"></path>
+      </svg>
+    </div>
+  );
+};
+
+export const TrendingUpIcon = (props) => {
+  const { className, ...otherProps } = props;
+
+  return (
+    <div className={className}>
+      <svg
+        {...otherProps}
+        xmlns="http://www.w3.org/2000/svg"
+        viewBox="0 0 24 24"
+      >
+        <path d="m10 10.414 4 4 5.707-5.707L22 11V5h-6l2.293 2.293L14 11.586l-4-4-7.707 7.707 1.414 1.414z"></path>
+      </svg>
+    </div>
+  );
+};
+
+export const TrendingDownIcon = (props) => {
+  const { className, ...otherProps } = props;
+
+  return (
+    <div className={className}>
+      <svg
+        {...otherProps}
+        xmlns="http://www.w3.org/2000/svg"
+        viewBox="0 0 24 24"
+      >
+        <path d="m14 9.586-4 4-6.293-6.293-1.414 1.414L10 16.414l4-4 4.293 4.293L16 19h6v-6l-2.293 2.293z"></path>
+      </svg>
+    </div>
+  );
+};
--- src/screens/dashboard/index.jsx
+++ src/screens/dashboard/index.jsx
@@ -12,6 +12,18 @@ import Chart from "chart.js/auto";
 import { CategoryScale } from "chart.js";
 import { useDispatch, useSelector } from "react-redux";
 import "../style.css";
+import {
+  DollarIcon,
+  SaveMoneyIcon,
+  TransferIcon,
+  TrendingDownIcon,
+  TrendingUpIcon,
+  WalletIcon,
+} from "../../components/icons";
+import LineChart from "../../components/lineChart";
+import PieChart from "../../components/donutChart";
+import DonutChart from "../../components/donutChart";
+import BarChart from "../../components/barChart";
 
 Chart.register(CategoryScale);
 
@@ -66,80 +78,147 @@ const Dashboard = () => {
 
   return (
     <>
-      <div className="d-flex gap-3 flex-lg-nowrap flex-wrap mt-3">
+      <div className="d-flex justify-content-evenly gap-3 flex-lg-nowrap flex-wrap mt-3">
         <Card className="w-25 w-lg-50">
-          <CardBody className="pb-0">
-            <CardTitle>Total Expense</CardTitle>
-          </CardBody>
-          <CardBody className="pt-0">
-            <CardText className="no-data">No Data found</CardText>
+          <CardBody>
+            <DollarIcon className="icon-with-bg" fill="#696cff" />
+            <small className="text-uppercase ">Total Income</small>
+            <h4 className="fw-semibold mt-1">Rs 45,000</h4>
+            <small className="d-flex align-items-center">
+              <TrendingUpIcon
+                width="22"
+                height="22"
+                fill="#696cff"
+                className="me-2"
+              />
+              <span className="text-primary fw-medium me-1">6%</span> vs last 07
+              days
+            </small>
           </CardBody>
         </Card>
 
         <Card className="w-25 w-lg-50">
-          <CardBody className="pb-0">
-            <CardTitle>Total Income</CardTitle>
-          </CardBody>
-          <CardBody className="pt-0">
-            <CardText className="no-data">No Data found</CardText>
+          <CardBody>
+            <TransferIcon className="icon-with-bg" fill="#696cff" />
+            <small className="text-uppercase">Total Expense</small>
+            <h4 className="fw-semibold">Rs 45,000</h4>
+            <small className="d-flex align-items-center">
+              <TrendingDownIcon
+                width="22"
+                height="22"
+                fill="#dc3545"
+                className="me-2"
+              />
+              <span className="text-danger fw-medium me-1">6%</span> vs last 07
+              days
+            </small>
           </CardBody>
         </Card>
 
         <Card className="w-25 w-lg-50">
-          <CardBody className="pb-0">
-            <CardTitle>Total Saving</CardTitle>
-          </CardBody>
-          <CardBody className="pt-0">
-            <CardText className="no-data">No Data found</CardText>
+          <CardBody>
+            <SaveMoneyIcon className="icon-with-bg" fill="#696cff" />
+            <small className="text-uppercase ">Total Saving</small>
+            <h4 className="fw-semibold">Rs 45,000</h4>
+            <small className="d-flex align-items-center">
+              <TrendingDownIcon
+                width="22"
+                height="22"
+                fill="#dc3545"
+                className="me-2"
+              />
+              <span className="text-danger fw-medium me-1">6%</span> vs last 07
+              days
+            </small>
           </CardBody>
         </Card>
 
         <Card className="w-25 w-lg-50">
-          <CardBody className="pb-0">
-            <CardTitle>Mostly Expense</CardTitle>
-          </CardBody>
-          <CardBody className="pt-0">
-            <CardText className="no-data">No Data found</CardText>
+          <CardBody>
+            <WalletIcon className="icon-with-bg" fill="#696cff" />
+            <small className="text-uppercase ">Mostly Spending</small>
+            <h4 className="fw-semibold">House Hold</h4>
+            <small className="d-flex align-items-center">
+              <TrendingUpIcon
+                width="22"
+                height="22"
+                fill="#696cff"
+                className="me-2"
+              />
+              <span className="text-primary fw-medium me-1">6%</span> vs last 07
+              days
+            </small>
           </CardBody>
         </Card>
       </div>
 
-      <div className="d-flex gap-3 my-3">
-        <Card className="w-63">
-          <CardBody className="pb-0">
-            <CardTitle>Bar Chart</CardTitle>
-          </CardBody>
-          <CardBody className="pt-0">
-            <CardText className="no-data">No Data found</CardText>
+      <div className="d-flex gap-3 flex-md-nowrap flex-wrap my-3">
+        <Card className="w-75">
+          <CardBody className="">
+            <BarChart />
           </CardBody>
         </Card>
 
-        <Card className="w-37">
-          <CardBody className="pb-0">
-            <CardTitle>Latest expenses</CardTitle>
-          </CardBody>
-          <CardBody className="pt-0">
-            <CardText className="no-data">No Data found</CardText>
+        <Card className="w-25">
+          <CardBody>
+            <CardTitle className="text-uppercase mb-3">
+              Recent Expenses
+            </CardTitle>
+            <div className="d-flex flex-column gap-3">
+              <div className="recent-expenses">
+                <div className="d-flex justify-content-between">
+                  <p className="m-0">Fridge</p>
+                  <p className="m-0">Rs 500</p>
+                </div>
+                <span>2 July, 2024</span>
+              </div>
+
+              <div className="recent-expenses">
+                <div className="d-flex justify-content-between">
+                  <p className="m-0">Fridge</p>
+                  <p className="m-0">Rs 500</p>
+                </div>
+                <span>2 July, 2024</span>
+              </div>
+
+              <div className="recent-expenses">
+                <div className="d-flex justify-content-between">
+                  <p className="m-0">Fridge</p>
+                  <p className="m-0">Rs 500</p>
+                </div>
+                <span>2 July, 2024</span>
+              </div>
+
+              <div className="recent-expenses">
+                <div className="d-flex justify-content-between">
+                  <p className="m-0">Fridge</p>
+                  <p className="m-0">Rs 500</p>
+                </div>
+                <span>2 July, 2024</span>
+              </div>
+            </div>
           </CardBody>
         </Card>
       </div>
 
-      <div className="d-flex gap-3 mb-3">
+      <div className="d-flex gap-3 flex-md-nowrap flex-wrap mb-3">
         <Card className="w-37">
-          <CardBody className="pb-0">
-            <CardTitle>Pie chart</CardTitle>
-          </CardBody>
-          <CardBody className="pt-0">
-            <CardText className="no-data">No Data found</CardText>
+          <CardBody className="">
+            <CardTitle className="text-uppercase mb-4">
+              Report Overview
+            </CardTitle>
+            <div className="d-flex">
+              <DonutChart />
+            </div>
           </CardBody>
         </Card>
 
         <Card className="w-63">
-          <CardBody className="pb-0">
-            <CardTitle>Line Chart of latest expenses</CardTitle>
-          </CardBody>
-          <CardBody className="pt-0">
-            <CardText className="no-data">No Data found</CardText>
+          <CardBody className="">
+            <CardTitle className="mb-4 text-uppercase">
+              Expenses Activity
+            </CardTitle>
+            <LineChart />
           </CardBody>
         </Card>
       </div>
